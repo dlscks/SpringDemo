ddbcea78ba48d1726f724a9c4ab84161f9253ff6
dlscks <dlscks971026@gmail.com>
2022-11-16 19:32:27 +0900

    20221116 | up

---
+++ spring_01_basic/src/sample01/MessageBeanKorea.java
@@ -0,0 +1,12 @@
+package sample01;
+
+public class MessageBeanKorea {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void sayHello(String name) {
+		System.out.printf("안녕, %s !!!", name);
+	}
+}
