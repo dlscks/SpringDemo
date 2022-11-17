5c8c6315c5aaeb9de57093cd8e6967cd88344113
dlscks <dlscks971026@gmail.com>
2022-11-17 21:40:44 +0900

    20221117 | up

--- spring_03_aop/src/main/java/part01_xml/ServiceImp.java
+++ spring_03_aop/src/main/java/part01_xml/ServiceImp.java
@@ -31,21 +31,28 @@ public class ServiceImp implements Service{
 
 	@Override
 	public void prn2() {
+		System.out.println("prn2");
 		
 	}
 
 	@Override
 	public String prn3() {
-		return null;
+		System.out.println("prn3");
+		return "홍길동";
 	}
 
 	@Override
 	public void prn4() {
-		
+		System.out.println("prn4");
+//		String data="123";
+		String data="abc";
+		int num = Integer.parseInt(data);
+		System.out.println("num:"+ num);
 	}
 
 	@Override
 	public void prn5() {
+		System.out.println("prn5");
 		
 	}
 
